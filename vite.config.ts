c1a8952763456e0acc3c3f2ce6c7a31d5d322aed
frank-515 <frank515_@outlook.com>
2023-05-30 20:41:19 -0700

    add alias @ as root of the project;

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src')
+      
+    },
+  },
   plugins: [
     vue(),
     electron([
