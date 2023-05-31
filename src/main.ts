e7a9abf02ae305ff78606c23b07fca824b00fba0
frank-515 <frank515_@outlook.com>
2023-05-30 20:50:08 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import { IonicVue } from '@ionic/vue';
 
-createApp(App).mount('#app').$nextTick(() => postMessage({ payload: 'removeLoading' }, '*'))
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+const pinia = createPinia()
+
+createApp(App)
+    .use(pinia)
+    .use(IonicVue)
+    .mount('#app')
+    .$nextTick(() => postMessage({ payload: 'removeLoading' }, '*'))
