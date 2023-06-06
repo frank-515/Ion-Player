97c92559f60c55842b652888316ade5e03de3b9d
frank-515 <frank515_@outlook.com>
2023-06-05 23:35:37 -0700

    update

--- electron/src/musicFinder.ts
+++ electron/src/musicFinder.ts
@@ -3,13 +3,13 @@ import * as path from 'path';
 import { parseFile }  from  'music-metadata' 
 
 
-async function getMusicMetadata(filename: string): Promise<{ artist: string | undefined, title: string | undefined}> {
+async function getMusicMetadata(filename: string): Promise<{ artist: string | undefined, title: string | undefined, duration: number | undefined}> {
   try {
     const metadata = await parseFile(filename);
-    return {artist: metadata.common.artist, title: metadata.common.title};
+    return {artist: metadata.common.artist, title: metadata.common.title, duration: metadata.format.duration};
   } catch (error:any) {
     console.error(error.message);
-    return {artist: undefined, title: undefined};
+    return {artist: undefined, title: undefined, duration: undefined};
   }
 }
 async function getMP3Cover(filepath: string) {
@@ -23,40 +23,33 @@ async function getMP3Cover(filepath: string) {
   }
 }
 
-/**
- * 递归遍历文件夹，返回符合正则表达式的文件名数组
- * Generate by gpt-3.5
- * @param {string} dir 目标文件夹路径
- * @param {RegExp} reg 指定正则表达式
- * @return {Array} 符合指定正则表达式的文件名数组
- */
-function getFileList(dir: string, reg: RegExp): string[] {
-    let fileList: string[] = [];
+interface File {
+  path: string;
+}
+
+function getFileList(dir: string, reg: RegExp): File[] {
+  let fileList: File[] = [];
   
-    try {
-      // 读取目录
-      const files: string[] = fs.readdirSync(dir);
+  try {
+    const files: string[] = fs.readdirSync(dir);
   
-      // 遍历目录下所有文件
-      for (const file of files) {
-        const filePath: string = path.join(dir, file);
+    for (const file of files) {
+      const filePath: string = path.join(dir, file);
   
-        // 如果是文件夹就递归遍历
-        if (fs.statSync(filePath).isDirectory()) {
-          fileList = fileList.concat(getFileList(filePath, reg));
-        } else {
-          // 如果是文件，则判断是否符合正则表达式
-          if (reg.test(file)) {
-            fileList.push(filePath);
-          }
+      if (fs.statSync(filePath).isDirectory()) {
+        fileList = fileList.concat(getFileList(filePath, reg));
+      } else {
+        if (reg.test(file)) {
+          fileList.push({ path: filePath });
         }
       }
-    } catch (err: any) {
-      console.error(`Something went wrong: ${err.message}`);
     }
-  
-    return fileList;
+  } catch (err: any) {
+    console.error(`Something went wrong: ${err.message}`);
   }
+  
+  return fileList;
+}
 
   export {
     getFileList, getMusicMetadata, getMP3Cover
