7d9b61d27038c754677c9b9ecf578842498a9c97
frank-515 <frank515_@outlook.com>
2023-06-05 23:35:20 -0700

    add backend APIs

--- electron/src/fileServer.ts
+++ electron/src/fileServer.ts
@@ -1,7 +1,20 @@
 import http from 'http';
 import fs from 'fs';
-import path from 'path';
 import {getFileList, getMP3Cover, getMusicMetadata} from './musicFinder'
+import DataStore from 'nedb'
+import { fromHomeDir } from '../../src/misc/util'
+let libraryDB: DataStore
+let playlistDB: DataStore
+const initDB = () => {
+  libraryDB = new DataStore({
+    filename: fromHomeDir("libraryLib.db"),
+    autoload: true
+  })
+  playlistDB = new DataStore({
+    filename: fromHomeDir("playlist.db"),
+    autoload: true
+  });
+}
 
 /**
  * Generates By GPT-3.5
@@ -15,8 +28,9 @@ const createServerWithMusicPath = (port: number): http.Server => {
   const server = http.createServer(function (req: any, res: any) {
     // 解析请求的 URL
     const url = decodeURIComponent(req.url);
-    console.log(url);
-    // 如果请求的是 /music/* 路径
+    console.log('Req ' + url);
+    // 感觉不如上express
+    // TODO : 欢迎来到地狱！我的朋友！
     if (url.startsWith('/music/')) {
       // 获取请求文件的绝对路径
       // const filePath = path.join(__dirname, url.slice('/music/'.length));
@@ -53,7 +67,9 @@ const createServerWithMusicPath = (port: number): http.Server => {
         // 文件存在，返回文件元数据
         getMusicMetadata(filePath).then(meta => {
           res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(meta);
+          res.write(JSON.stringify(meta));
+          // Why I need to invoke end() here but others don't?
+          res.end()
         })
         .catch(err => {console.error(err)})
       });
@@ -79,7 +95,179 @@ const createServerWithMusicPath = (port: number): http.Server => {
         .catch(err => {console.error(err)})
         
       });
-    } else  {
+    } else if (url.startsWith('/add-lib/')) {
+      const path = url.replace(/^\/add-lib\//, '/')
+      
+      const filePaths = getFileList(path, /\.mp3$/i)
+
+      interface MusicData {
+        path: string;
+        metadata: {title: string| undefined, artist: string | undefined, duration: number | undefined},
+        liked: boolean;
+      }
+
+      const data: MusicData[] = []
+      filePaths.forEach(element => {
+        getMusicMetadata(element.path)
+        .then(metadata => {
+          data.push({path:element.path, metadata:metadata, liked: false})
+        })
+      });
+      setTimeout(() => {
+        libraryDB.insert(data, function (err, newDocs) {
+          if (err) {
+            console.error(err);
+            res.writeHead(500)
+            res.write('{count: ' + 0 + '}')    
+            return res.end();
+          } else {
+            res.writeHead(200, 'application/json')
+            res.write('{count: ' + newDocs.length + '}')
+            return res.end()
+          }
+        })
+      }, 100)
+    } else if (url.startsWith('/get-lib')) {
+      libraryDB.find({}, function(err: any, docs: any) {
+        if (err) {
+          console.error(err);
+          res.writeHead(500)
+          return res.end();
+        } else {
+          res.writeHead(200, { 'Content-Type': 'application/json' })
+          res.write(JSON.stringify(docs))
+          return res.end();
+        }
+      })
+    } else if (url.startsWith('/like/')) { // Deprecated
+      const title = url.replace(/^\/like\//, '')
+      // libraryDB.findOne({"metadata.title": title}, (err: any, docs: any) => {
+      //   if (err) {
+      //     console.error(err);
+      //     res.writeHead(500)
+      //     return res.end()
+      //   }
+      //   console.log(docs);
+        
+      //   libraryDB.update({_id: docs._id}, {$set: {liked: !docs}})
+      //   res.writeHead(200, {'content-type': 'application/json'})
+      //   res.write(JSON.stringify(docs))
+      //   res.end()
+      // })
+      libraryDB.update({ 'metadata.title': title }, { $set: { liked: { $not: '$liked' } } }, { multi: true }, function(err, numAffected, affectedDocuments) {
+        if (err) {
+          console.log('Update failed:', err);
+          return;
+        }
+        console.log('Number of documents matched and updated:', numAffected);
+      });
+
+    } else if (url.startsWith('/search/')) {
+      const target = url.split('/')[3]
+      const type = url.split('/')[2] // name, artist, album
+      
+      const regex = new RegExp(target, 'i')
+
+      if (type == 'name' ) {
+        libraryDB.find({ "metadata.title": { $regex: regex } }, function (err: any, docs: any) {
+        if (err) {
+          console.error(err);
+          res.writeHead(500)
+          return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json'})
+        res.write(JSON.stringify(docs))
+        res.end();
+      })
+    } else if (type == 'artist') {
+        libraryDB.find({ "metadata.artist": { $regex: regex} }, function (err: any, docs: any) {
+          if (err) {
+            console.error(err);
+            res.writeHead(500)
+            return res.end();
+          }
+          res.writeHead(200, { 'Content-Type': 'application/json'})
+          res.write(JSON.stringify(docs))
+          res.end();
+        })
+      };
+    } else if (url.startsWith('/add-playlist')) { // POST
+      // Generate by GPT-3.5
+      let postData = ''
+      req.on('data', (chunk: any) => {
+        postData += chunk.toString(); // 拼接 POST 请求数据
+      });
+      req.on('end', () => {
+        // console.log("DATA:"+postData); // 打印 POST 请求数据
+  
+        const postDataObj = JSON.parse(postData); // 解析 POST 请求数据为 JSON 对象
+        // console.log("TO JSON:"+postDataObj); // 打印 POST 请求数据的 JSON 对象
+        playlistDB.insert(postDataObj, function(err: any, docs: any) {
+          if (err) {
+            console.error(err);
+            res.writeHead(500)
+            return res.end();
+          } 
+          res.writeHead(200, {'Content-Type': 'application/json'}); // 设置响应头信息
+          res.end(JSON.stringify({status: 'ok'})); // 返回响应结果
+        })
+        
+      });
+    } else if (url.startsWith('/add2playlist')) { // {playlistName, songPath}. POST
+      let postData = ''
+      req.on('data', (chunk: any) => {
+        postData += chunk.toString(); // 拼接 POST 请求数据
+      });
+      req.on('end', () => {
+        // console.log("DATA:"+postData); // 打印 POST 请求数据
+        
+        const postDataObj = JSON.parse(postData); // 解析 POST 请求数据为 JSON 对象
+
+        getMusicMetadata(postDataObj.songPath)
+          .then(meta => {
+            playlistDB.update(
+              { playlistName: postDataObj.playlistName }, // 匹配条件
+              { $push: { playlist: {path: postDataObj.songPath, metadata: meta} } }, // 更新操作，向 playlist 数组中插入 testSong 元素
+              { upsert: true }, // 如果查找不到相应的文档，则插入一个新文档
+              function (err: any) {
+                if (err) {
+                  console.error(err);
+                  res.writeHead(500)
+                  return res.end();
+                }
+                res.writeHead(200, {'Content-Type': 'application/json'}); // 设置响应头信息
+                res.end(JSON.stringify({status: 'ok'})); // 返回响应结
+            });
+          })
+          .catch(err => {console.error(err)})
+      })      
+    } else if (url.startsWith('/get-playlist')) { // 获取全部信息
+      playlistDB.find({}, {playlistName: 1, description: 1}, function (err, docs) {
+        if (err) {
+          res.writeHead(500);
+          console.error(err);
+          return res.end();
+        }
+        res.writeHead(200);
+        res.write(JSON.stringify(docs));
+        res.end();
+      })
+    } else if (url.startsWith('/playlist')) { // + playlistName
+      const playlist = url.split('/')[2]
+      console.log(playlist);
+      
+      playlistDB.findOne({playlistName: playlist}, function(err: any, docs: any) {
+        if (err) {
+          res.writeHead(500);
+          console.error(err);
+          return res.end()
+        }
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(docs));
+        res.end()
+      })
+
+    } else {
       // 如果请求的是其他路径，则返回 404
       res.writeHead(404);
       res.write(`URL '${url}' not found!`);
@@ -99,6 +287,6 @@ const createServerWithMusicPath = (port: number): http.Server => {
 // 如何从vite获取端口号?
 
 export {
-  createServerWithMusicPath
+  createServerWithMusicPath, initDB
 }
 
